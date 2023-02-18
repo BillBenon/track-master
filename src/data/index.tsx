dc81d22f435456be917e09748a0d5d10327c08de
Dushime Bill Benon <DushimeBillBenon@gmail.com>
2023-02-18 23:40:43 +0200

    [feat] all the pages implemented, remaining with responsiveness

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,5 +1,9 @@
 import generalData from "./generalData.json";
 import totalVisitsData from "./totalVisitsData.json";
 import overallVisits from "./overallVisits.json";
+import creditsData from "./creditsData.json";
+import transactionsData from "./transactionsData.json";
+import articlesData from "./articles.json";
+import guidesData from "./guides.json";
 
-export { generalData, totalVisitsData, overallVisits };
+export { generalData, totalVisitsData, overallVisits, creditsData, transactionsData, articlesData, guidesData };
